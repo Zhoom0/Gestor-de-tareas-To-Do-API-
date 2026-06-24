3d80a4369d06dc9fee8d69479041ea8ce1694c19
Zhoom0 <neumatex0@gmail.com>
2026-06-24 03:27:19 -0400

    Crear metodo (PUT) para actualizar una tarea y se crearon clases correspondientes para distintos response_model

--- routers/tareas.py
+++ routers/tareas.py
@@ -12,8 +12,26 @@ class Tarea(BaseModel):
     prioridad: int = Field(ge=1, le=5)
     completada: bool = Field(default=False)
 
+class Tarea_actualizar(BaseModel):
+    titulo: str = Field(min_length=1, max_length=100)
+    descripcion: str | None = Field(default=None)
+    prioridad: int = Field(ge=1, le=5)
+    completada: bool = Field(default=False)
+
+class Tarea_respuesta(BaseModel):
+    titulo: str = Field(min_length=1, max_length=100)
+    descripcion: str | None = Field(default=None)
+    prioridad: int = Field(ge=1, le=5)
+    completada: bool = Field(default=False)
+
+class Respuesta_actualizacion(BaseModel):
+    mensaje: str
+    tarea_antigua: Tarea_respuesta
+    tarea_actualizada: Tarea_respuesta
+
+
 # GET
-# TODAS LAS TAREAS
+# Ver todas las tareas
 @router.get("")
 def mostrar_tareas():
     return tareas
@@ -27,7 +45,19 @@ def mostrar_tareaid(tarea_id: int):
     raise HTTPException (status_code=status.HTTP_404_NOT_FOUND, detail="Tarea no encontrada")
 
 # POST
+# Crear tarea
 @router.post("", status_code=status.HTTP_201_CREATED)
 def crear_tarea(tarea: Tarea):
     tareas.append(tarea.model_dump())
     return {"mensaje": "se ha creado la tarea exitosamente"}
+
+# PUT
+# Actualizar tarea
+@router.put("/{tarea_id}", response_model=Respuesta_actualizacion)
+def actualizar_tarea(tarea_id: int, tarea_nueva: Tarea_actualizar):
+    for indice, tarea in enumerate(tareas):
+        if tarea["id"] == tarea_id:
+            tarea_antigua = tareas[indice].copy()
+            tareas[indice] = {**tarea_nueva.model_dump(), "id": tarea_id}
+            return {"mensaje": "la tarea se ha modificado exitosamente", "tarea_antigua": tarea_antigua, "tarea_actualizada": tareas[indice]}
+    raise HTTPException(status_code=404, detail="Tarea no encontrada")
