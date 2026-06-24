6542bd59d9ae50940519557fed3e0de1944d66f3
Zhoom0 <neumatex0@gmail.com>
2026-06-24 04:43:48 -0400

    Corregír PascalCase en los modelos

--- routers/tareas.py
+++ routers/tareas.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException, status
-from pydantic import BaseModel, Field, EmailStr
+from pydantic import BaseModel, Field
 
 router = APIRouter()
 
@@ -12,48 +12,48 @@ class Tarea(BaseModel):
     prioridad: int = Field(ge=1, le=5)
     completada: bool = Field(default=False)
 
-class Tarea_crear(BaseModel):
+class TareaCrear(BaseModel):
     titulo: str = Field(min_length=1, max_length=100)
     descripcion: str | None = Field(default=None)
     prioridad: int = Field(ge=1, le=5)
     completada: bool = Field(default=False)
 
-class Tarea_actualizar(BaseModel):
+class TareaActualizar(BaseModel):
     titulo: str = Field(min_length=1, max_length=100)
     descripcion: str | None = Field(default=None)
     prioridad: int = Field(ge=1, le=5)
     completada: bool = Field(default=False)
 
-class Tarea_respuesta(BaseModel):
+class TareaRespuesta(BaseModel):
     titulo: str = Field(min_length=1, max_length=100)
     descripcion: str | None = Field(default=None)
     prioridad: int = Field(ge=1, le=5)
     completada: bool = Field(default=False)
 
-class Respuesta_actualizacion(BaseModel):
+class RespuestaActualizacion(BaseModel):
     mensaje: str
-    tarea_antigua: Tarea_respuesta
-    tarea_actualizada: Tarea_respuesta
+    tarea_antigua: TareaRespuesta
+    tarea_actualizada: TareaRespuesta
 
 
 # GET
 # Ver todas las tareas
-@router.get("", response_model=list[Tarea_respuesta])
+@router.get("", response_model=list[TareaRespuesta])
 def mostrar_tareas():
     return tareas
 
 # Mostrar solo tareas completadas
-@router.get("/completadas", response_model=list[Tarea_respuesta])
+@router.get("/completadas", response_model=list[TareaRespuesta])
 def mostrar_completadas():
     return [tarea for tarea in tareas if tarea["completada"]]
 
 # Mostrar solo tareas no completadas
-@router.get("/no_completadas", response_model=list[Tarea_respuesta])
+@router.get("/no_completadas", response_model=list[TareaRespuesta])
 def mostrar_no_completadas():
     return [tarea for tarea in tareas if not tarea["completada"]]
 
 # Buscar tarea por id
-@router.get("/{tarea_id}", response_model=Tarea_respuesta)
+@router.get("/{tarea_id}", response_model=TareaRespuesta)
 def mostrar_tareaid(tarea_id: int):
     for tarea in tareas:
         if tarea["id"] == tarea_id:
@@ -63,15 +63,15 @@ def mostrar_tareaid(tarea_id: int):
 # POST
 # Crear tarea
 @router.post("", status_code=status.HTTP_201_CREATED)
-def crear_tarea(tarea: Tarea_crear):
+def crear_tarea(tarea: TareaCrear):
     nuevo_id = max([t["id"] for t in tareas], default=0) + 1
     tareas.append({**tarea.model_dump(), "id": nuevo_id})
     return {"mensaje": "se ha creado la tarea exitosamente"}
 
 # PUT
 # Actualizar tarea
-@router.put("/{tarea_id}", response_model=Respuesta_actualizacion)
-def actualizar_tarea(tarea_id: int, tarea_nueva: Tarea_actualizar):
+@router.put("/{tarea_id}", response_model=RespuestaActualizacion)
+def actualizar_tarea(tarea_id: int, tarea_nueva: TareaActualizar):
     for indice, tarea in enumerate(tareas):
         if tarea["id"] == tarea_id:
             tarea_antigua = tareas[indice].copy()
