80faa460bff109e83b8afd25e18165c73f15c8a2
Zhoom0 <neumatex0@gmail.com>
2026-06-24 03:35:46 -0400

    Agregar endpoint (DELETE) para borrar una tarea

--- routers/tareas.py
+++ routers/tareas.py
@@ -37,11 +37,11 @@ def mostrar_tareas():
     return tareas
 
 # BUSCAR TAREA POR ID
-@router.get("/{tarea_id}")
+@router.get("/{tarea_id}", response_model=Tarea_respuesta)
 def mostrar_tareaid(tarea_id: int):
     for tarea in tareas:
         if tarea["id"] == tarea_id:
-            return {"mensaje": "tarea encontrada", "tarea": tarea}
+            return tarea
     raise HTTPException (status_code=status.HTTP_404_NOT_FOUND, detail="Tarea no encontrada")
 
 # POST
@@ -60,4 +60,14 @@ def actualizar_tarea(tarea_id: int, tarea_nueva: Tarea_actualizar):
             tarea_antigua = tareas[indice].copy()
             tareas[indice] = {**tarea_nueva.model_dump(), "id": tarea_id}
             return {"mensaje": "la tarea se ha modificado exitosamente", "tarea_antigua": tarea_antigua, "tarea_actualizada": tareas[indice]}
+    raise HTTPException(status_code=404, detail="Tarea no encontrada")
+
+# DELETE
+# Borrar una tarea
+@router.delete("/{tarea_id}")
+def borrar_tarea(tarea_id: int):
+    for indice, tarea in enumerate(tareas):
+        if tarea["id"] == tarea_id:
+            tareas.pop(indice)
+            return {"mensaje": "la tarea se ha eliminado exitosamente"}
     raise HTTPException(status_code=404, detail="Tarea no encontrada")
